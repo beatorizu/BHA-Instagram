58e2b4131b236925e4595289a826cbabca80f617
beatorizu <betrizaf@gmail.com>
2020-05-27 17:09:55 -0300

    fix: Show the right user on the profile page

--- src/routes/ProfileRoute/ProfileRoute.jsx
+++ src/routes/ProfileRoute/ProfileRoute.jsx
@@ -6,16 +6,17 @@ import UserPosts from '../../containers/UserPosts';
 import Loading from '../../components/Loading';
 
 import postsMock from '../../__tests__/bha_mocks/posts';
+import usersMock from '../../__tests__/bha_mocks/users';
 
 const ProfileRoute = () => {
   const username = window.location.pathname.split('/').pop()
-  const avatar = "/img/profiles/momo-yaoyorozu/momo-yaoyorozu-profile.png"
-  const name = username.charAt(0).toUpperCase() + username.slice(1)
+  const user = usersMock.find(user => user.username === username)
+  const posts = postsMock.filter(post => post.userId === user.id)
 
   return (
     <div>
-      <UserProfile username={username} avatar={avatar} name={name} />
-      <UserPosts posts={postsMock} />
+      <UserProfile username={username} avatar={user.avatar} name={user.name} />
+      <UserPosts posts={posts} />
     </div>
   );
 };
