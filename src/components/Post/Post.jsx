eb47d9548336d9c6bacdea2fb3675189b0de1165
beatorizu <betrizaf@gmail.com>
2020-05-28 16:55:45 -0300

    feat: Show who liked the post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,16 +3,24 @@ import { Link } from 'react-router-dom';
 
 import './Post.scss';
 
+import usersMock from '../../__tests__/bha_mocks/users';
+const getPostUserById = postUserId => usersMock.find(user => postUserId === user.id)
+
 const Post = ({ postInfo, userInfo }) => {
+  const { imageUrl, likes } = postInfo;
+  const { username, name } = getPostUserById(likes[0].id);
+
   return (
     <article className="post" data-testid="post">
       {userInfo && (
         <header className="post__header">
           <div className="user">
             <Link className="user__thumb" to={`/users/${userInfo.username}`}>
-              <img src={userInfo.avatar} alt=""/>
+              <img src={userInfo.avatar} alt="" />
             </Link>
-            <Link className="user__name" to={`/users/${userInfo.username}`}>{userInfo.name}</Link>
+            <Link className="user__name" to={`/users/${userInfo.username}`}>
+              {userInfo.name}
+            </Link>
           </div>
           <button className="post__context">
             <span className="follow-btn">Seguir</span>
@@ -20,7 +28,7 @@ const Post = ({ postInfo, userInfo }) => {
         </header>
       )}
       <figure className="post__figure">
-        <img src={postInfo.imageUrl} alt="" />
+        <img src={imageUrl} alt="" />
       </figure>
       {userInfo && (
         <nav className="post__controls">
@@ -29,7 +37,15 @@ const Post = ({ postInfo, userInfo }) => {
           </button>
           <div className="post__status">
             <div className="user">
-              <span>curtido por <a href="/">Santino Rowe</a> e outra <a href="/">1 pessoa.</a></span>
+              {
+                <span>
+                  curtido por <Link to={`/users/${username}`}>{name}</Link>
+                  {likes.length - 1 >= 1 &&
+                    ` e outra${likes.length - 1 > 1 ? "s" : ""} pessoa${
+                      likes.length - 1 > 1 ? "s" : ""
+                    }`}
+                </span>
+              }
             </div>
           </div>
         </nav>
