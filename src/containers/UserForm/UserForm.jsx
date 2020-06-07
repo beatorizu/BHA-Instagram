97492b1e0e1a114d83f5c2ca35dc609a9c84a41c
beatorizu <betrizaf@gmail.com>
2020-06-07 20:51:59 -0300

    feat: Preview new account

--- src/containers/UserForm/UserForm.jsx
+++ src/containers/UserForm/UserForm.jsx
@@ -7,6 +7,20 @@ import './UserForm.scss';
 const UserForm = () => {
   return (
     <React.Fragment>
+      <section className="profile" data-testid="user-profile">
+        <div className="container">
+          <div className="profile-data">
+            <div className="user">
+              <div className="user__thumb--newstory">
+                <div className="user__thumb">
+                  <img src="" alt="" />
+                </div>
+              </div>
+              <p className="user__name">blha<span>@blha</span></p>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="post__form" data-testid="user-form">
         <div className="post__form__wrapper">
           <label>Nome</label>
